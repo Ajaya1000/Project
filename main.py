6ea463d09cd0f5627fc2eb821d1d671b97b8aa6a
@itsAj <ajaymati1000@gmail.com>
2020-12-27 09:38:14 +0530

    model training completed

--- main.py
+++ main.py
@@ -169,6 +169,27 @@ def define_model(vocab_size,max_length):
 
 
 
+#train our model
+print('Dataset: ',len(train_imgs))
+print('Description: train=',len(train_descriptions))
+print('Photos: train=',len(train_features))
+print('Vocabulary Size:',vocab_size)
+print('Description Length: ',max_length)
+
+
+model = define_model(vocab_size,max_length)
+epochs = 10
+
+steps = len(train_descriptions)
+#making a directory moels to save out models
+os.mkdir("models")
+for i in range(epochs):
+    generator = data_generator(train_descriptions,train_features,tokenizer,max_length)
+    
+    model.fit_generator(generator,epochs=1,steps_per_epoch=steps,verbose=1)
+    
+    model.save("models/model_"+str(i)+".h5")
+
 
 
 
